0bc7febd84921a2e8bf516e5bd896e4b5f5fd230
23paisow <59451279+23paisow@users.noreply.github.com>
2020-01-22 11:02:23 -0500

    Add files via upload

---
+++ pais_madlib2.py
@@ -0,0 +1,16 @@
+name = input("Enter a name: ")
+adj = input("Enter an adjective: ")
+adj2 = input("Enter an adjective: ")
+ad = input("Enter an adverb: ")
+foo = input("Enter a food: ")
+foo2 = input("Enter another food: ")
+noun = input("Enter a noun: ")
+game = input("Enter a game: ")
+verb = input("Enter a verb: ")
+
+
+
+print(name + " loves to sit and play " + game + name +  " also like to eat " + foo + "." )
+print(noun + " is the place I like to go" " I like " + adj + foo2 + " ALOT ")
+print("\n")
+print(verb + " ALOT " " I also want a " + ad + " The End ")
---
+++ pais_unit1_practice.py
@@ -0,0 +1,46 @@
+print("Welcome to Information Technology\n\nProgramming and\nWeb Development")
+print("        ___________\n| |     / /_  __/   |\n| | /| / / / / / /| |\n| |/ |/ / / / / ___ |\n|__/|__/ /_/ /_/  |_|")
+print("**********\n*        *\n*        *\n* MR. G  *\n* RM 119 *\n*        *\n**********")
+print("Computer Science is no more about computers\nthan astronomy is about telescopes\n- Edsger W. Dijkstra")
+
+user_name = input("What is your name?")
+favorite_animal = input("What is your favorite animal?")
+number = input("What is your favorite number")
+print(number * 3)
+sumResults = 5 + 6
+quotientResult = 5 / 6
+firstNUM = int(input("Enter a number:  "))
+
+length = int(input("Enter a number:  "))
+width = int(input("Enter a number:  "))
+
+
+print("  ",length,"\n\n")
+print(" ------")
+print("|", "      ", "|")
+print("|", "      ", "|")
+print("|", "      ", "|")
+print(" ------")
+print("\n\nArea =", length * width)
+
+num = input("Enter a number: ")
+print(num * 8)
+a = input("Enter an animal: ")
+s = input ("Enter a sound:  ")
+e = "E-I-E-I-O"
+
+
+print ("Old Macdonald had a farm, " + e)
+print ("And on his farm he had a " + a + "," + e)
+print ("With a " + a + "-" + a + " here and a" + s + "-" + s + " there")
+print ("Here a "+ s+ " there a " + s )
+print ("Everywhere a"  + s + "-" +a )
+print ("Old Macdonald had a farm," + e )
+
+
+
+name = input ("Enter a name") 
+adj = input ("Enter an adjective")
+adj = input ("Enter an adjective")
+s = input ("Enter an adverb")
+t = input 
