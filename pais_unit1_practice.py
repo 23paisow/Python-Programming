5dd70342ed524c42b6029bd349401a7a038b7512
23paisow <59451279+23paisow@users.noreply.github.com>
2020-01-24 13:46:19 -0500

    Add files via upload

--- pais_unit1_practice.py
+++ pais_unit1_practice.py
@@ -1,11 +1,19 @@
+#PART A
+
 print("Welcome to Information Technology\n\nProgramming and\nWeb Development")
 print("        ___________\n| |     / /_  __/   |\n| | /| / / / / / /| |\n| |/ |/ / / / / ___ |\n|__/|__/ /_/ /_/  |_|")
+
+#PART B
+
+print("A lighting flash:\nbetween the forest trees\nI have seen water.\n\t- Shiki")
 print("**********\n*        *\n*        *\n* MR. G  *\n* RM 119 *\n*        *\n**********")
 print("Computer Science is no more about computers\nthan astronomy is about telescopes\n- Edsger W. Dijkstra")
 
-user_name = input("What is your name?")
-favorite_animal = input("What is your favorite animal?")
-number = input("What is your favorite number")
+#PART C
+
+user_name = input("What is your name? ")
+favorite_animal = input("What is your favorite animal? ")
+number = input("What is your favorite number ")
 print(number * 3)
 sumResults = 5 + 6
 quotientResult = 5 / 6
@@ -17,14 +25,21 @@ width = int(input("Enter a number:  "))
 
 print("  ",length,"\n\n")
 print(" ------")
-print("|", "      ", "|")
-print("|", "      ", "|")
-print("|", "      ", "|")
+print("|", "    ", "|")
+print("|", "    ", "|")
+print("|", "    ", "|")
 print(" ------")
 print("\n\nArea =", length * width)
 
+#PART D
+
+name = input ("Please input your first name: ")
+last = input("Please input your last name: ")
 num = input("Enter a number: ")
 print(num * 8)
+
+#PART E
+
 a = input("Enter an animal: ")
 s = input ("Enter a sound:  ")
 e = "E-I-E-I-O"
@@ -32,15 +47,7 @@ e = "E-I-E-I-O"
 
 print ("Old Macdonald had a farm, " + e)
 print ("And on his farm he had a " + a + "," + e)
-print ("With a " + a + "-" + a + " here and a" + s + "-" + s + " there")
+print ("With a " + a + "-" + a + " here and a " + s + "-" + s + " there")
 print ("Here a "+ s+ " there a " + s )
-print ("Everywhere a"  + s + "-" +a )
+print ("Everywhere a "  + s + "-" +a )
 print ("Old Macdonald had a farm," + e )
-
-
-
-name = input ("Enter a name") 
-adj = input ("Enter an adjective")
-adj = input ("Enter an adjective")
-s = input ("Enter an adverb")
-t = input 
